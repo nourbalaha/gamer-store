1db62f99d009c37753e30bd3891864a890361696
Nour Balaha <nourbalaha0@gmail.com>
2019-12-17 17:11:42 +0900

    initial commit

---
+++ src/data/inventory.js
@@ -0,0 +1,60 @@
+const inventory = [
+    {
+        id: 1,
+        name:"bananas",
+        quantity: 200,
+        unit: "oz",
+        image: "https://images.freeimages.com/images/large-previews/fbd/bananas-with-honey-1639306.jpg",
+        category: "fruit"
+    },
+    {
+        id: 2,
+        name:"tomatos",
+        quantity: 10,
+        unit: "lb",
+        image: "https://images.freeimages.com/images/large-previews/8d0/tomatos-1326853.jpg",
+        category: "vegitables"
+    },
+    {
+        id: 3,
+        name:"carrots",
+        quantity: 13,
+        unit: "lb",
+        image: "https://images.freeimages.com/images/large-previews/503/carrots-1639017.jpg",
+        category: "vegitables"
+    },
+    {
+        id: 4,
+        name:"eggs",
+        quantity: 31,
+        unit: "dozen",
+        image: "https://images.freeimages.com/images/large-previews/d58/eggs-1561798.jpg",
+        category: "protiens"
+    },
+    {
+        id: 5,
+        name:"broccoli",
+        quantity: 30,
+        unit: "oz",
+        image: "https://images.freeimages.com/images/large-previews/7d1/delicious-vitamins-1328600.jpg",
+        category: "vegitables"
+    },
+    {
+        id: 6,
+        name:"butter",
+        quantity: 1,
+        unit: "lb",
+        image: "https://images.freeimages.com/images/large-previews/277/butter-2-1328164.jpg",
+        category: "dairy"
+    },
+    {
+        id: 7,
+        name:"milk",
+        quantity: 1,
+        unit: "gal",
+        image: "https://images.freeimages.com/images/large-previews/6f0/spilled-milk-1326993.jpg",
+        category: "dairy"
+    },
+];
+
+export default inventory; 
