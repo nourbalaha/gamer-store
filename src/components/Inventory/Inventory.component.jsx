375b7e762d6a00e93709302f22340f246e0e0039
Nour Balaha <nourbalaha0@gmail.com>
2019-12-18 16:05:32 +0900

    modified inventory component and added style

---
+++ src/components/Inventory/Inventory.component.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import Item from "../Item/Item.component"
+
+import inventory from "../../data/inventory"
+
+import "../inventory/Inventory.style.scss"
+
+export default class Inventory extends Component {
+    render() {
+        return (
+            <div className="inventory">
+                {
+                    inventory.map(item => <Item key={item.id} id={item.id} name={item.name} quantity={item.quantity} price= {item.price} image={item.image} platform={item.platform}/>)
+                }
+          </div>
+        )
+    }
+}
