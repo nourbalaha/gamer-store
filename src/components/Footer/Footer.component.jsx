c7e20041ed9234bbc1ea93f444f59a85eddb0ad8
Nour Balaha <nourbalaha0@gmail.com>
2019-12-20 19:42:35 +0900

    created footer component

---
+++ src/components/Footer/Footer.component.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import "./Footer.Style.scss"
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <p>© 2019 Nour Balaha</p>
+            </div>
+        )
+    }
+}
