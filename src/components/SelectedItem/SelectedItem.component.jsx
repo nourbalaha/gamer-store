1c0ddf9259d286fdf2cb6a07c50fc4c78aa1d09b
Nour Balaha <nourbalaha0@gmail.com>
2019-12-20 19:43:22 +0900

    created selected item component

---
+++ src/components/SelectedItem/SelectedItem.component.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+import "./SelectedItem.style.scss"
+
+import inventory from "../../data/inventory";
+
+export default class SelectedItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: inventory,
+      id:this.props.match.params.id
+    };
+  }
+
+  render() {
+    const item = this.state.items[this.state.id-1]
+    return (
+      <div className="selected-item">
+        <h2 className="item-name">{item.name}</h2>
+        <img
+          className="item-image"
+          src={item.image}
+          alt={item.name}
+        />
+        <p className="item-price">{item.price}$</p>
+        <p className="item-platform">{item.platform}</p>
+      </div>
+    );
+  }
+}
