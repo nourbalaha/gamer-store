62dd1ef7b79ea89b844a19fce43fa54b698cc0e3
Nour Balaha <nourbalaha0@gmail.com>
2019-12-20 19:47:55 +0900

    wip

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import "./App.css";
 
-import Inventory from "./components/inventory/Inventory.component";
+import Inventory from "./components/Inventory/Inventory.component";
 import Navbar from "./components/Navbar/Navbar.component";
+import SelectedItem from "./components/SelectedItem/SelectedItem.component"
+import Footer from "./components/Footer/Footer.component"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-      </header>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+        </header>
 
-      <main>
-        <p>categories</p>
-        <input id="search" name="search" type="text" placeholder="search" />
-        <Inventory />
-      </main>
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <p>platform</p>
+              <input
+                id="search"
+                name="search"
+                type="text"
+                placeholder="search"
+              />
+              <Inventory />
+            </Route>
+            <Route path="/:id" component={SelectedItem} />
+          </Switch>
+        </main>
 
-      <footer>footer</footer>
-    </div>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    </Router>
   );
 }
 
