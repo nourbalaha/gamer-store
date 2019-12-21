800dbf7028cd506c280d71aaa11b51cb79efd1f6
Nour Balaha <nourbalaha0@gmail.com>
2019-12-21 13:55:32 +0900

    modified item style

--- src/components/Item/Item.component.jsx
+++ src/components/Item/Item.component.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 import "./Item.style.scss";
 
-export default class Item extends Component {
+class Item extends Component {
   render() {
-    const { id, name, price, image, quantity, platform } = this.props;
+    const { id, name, price, image, platform } = this.props;
     return (
-      <Link to={`/${id}`} className="item">
+      <div onClick={()=>this.props.history.push(`/${id}`)} className="item">
         <img className="item-image" src={image} alt={name} />
         <span className="item-name">{name}</span>
-        <span className="item-price">{price}$</span>
         <span className="item-platform">{platform}</span>
-      </Link>
+        <span className="item-price">{price}$</span>
+      </div>
     );
   }
 }
+
+export default withRouter(Item);
