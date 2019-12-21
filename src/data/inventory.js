0f19ee1a43a328f776bb896e873b8fcad512f65c
Nour Balaha <nourbalaha0@gmail.com>
2019-12-21 19:11:58 +0900

    updated data

--- src/data/inventory.js
+++ src/data/inventory.js
@@ -1,7 +1,7 @@
 const inventory = [
     {
         id: 1,
-        name:"Persona 5",
+        name:"persona 5",
         quantity: 5,
         price: 40,
         image: "https://images-na.ssl-images-amazon.com/images/I/81jGL42j4GL._SL1500_.jpg",
@@ -9,21 +9,21 @@ const inventory = [
     },
     {
         id: 2,
-        name:"Marvel's Spider-Man",
+        name:"marvel's spider-Man",
         quantity: 5,
         price: 40,
         image: "https://images-na.ssl-images-amazon.com/images/I/81d6JU6g1pL._SL1500_.jpg",
         platform: "Playstation 4"
     },    {
         id: 3,
-        name:"Dark Souls III",
+        name:"dark souls III",
         quantity: 5,
         price: 40,
         image: "https://images-na.ssl-images-amazon.com/images/I/51zjwZ8Tq-L.jpg",
         platform: "Playstation 4"
     },    {
         id: 4,
-        name:"Grand Theft Auto V",
+        name:"grand theft auto v",
         quantity: 5,
         price: 60,
         image: "https://images-na.ssl-images-amazon.com/images/I/814zd8nG6IL._SL1266_.jpg",
