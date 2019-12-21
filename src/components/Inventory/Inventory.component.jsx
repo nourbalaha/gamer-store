21be4963c2582f7dd8b95d2ad1bdd3f88df1e04f
Nour Balaha <nourbalaha0@gmail.com>
2019-12-21 13:56:35 +0900

    updated inventory style

--- src/components/Inventory/Inventory.component.jsx
+++ src/components/Inventory/Inventory.component.jsx
@@ -1,19 +1,31 @@
 import React, { Component } from 'react'
 
-import Item from "../Item/Item.component"
+import Item from '../Item/Item.component'
 
-import inventory from "../../data/inventory"
+import inventory from '../../data/inventory'
 
-import "../Inventory/Inventory.style.scss"
+import '../Inventory/Inventory.style.scss'
 
 export default class Inventory extends Component {
-    render() {
-        return (
-            <div className="inventory">
-                {
-                    inventory.map(item => <Item key={item.id} id={item.id} name={item.name} quantity={item.quantity} price= {item.price} image={item.image} platform={item.platform}/>)
-                }
-          </div>
-        )
-    }
+  render () {
+    return (
+      <div className="wrapper">
+        <p>platform</p>
+        <input id='search' name='search' type='text' placeholder='search' />
+        <div className='inventory'>
+          {inventory.map(item =>
+            <Item
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              quantity={item.quantity}
+              price={item.price}
+              image={item.image}
+              platform={item.platform}
+            />
+          )}
+        </div>
+      </div>
+    )
+  }
 }
