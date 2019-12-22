94a10d51e4715d1fecccb491d36bd368880d7dab
Nour Balaha <nourbalaha0@gmail.com>
2019-12-22 16:53:13 +0900

    added homepage component

---
+++ src/pages/Home/Home.component.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import "./Home.style.scss"
+
+import gamingConsole from "../../assets/gaming-console.png"
+
+export default class Home extends Component {
+    handleClick=()=>{
+        this.props.history.push("/inventory")
+    }
+    render() {
+        return (
+            <div className="home-page">
+                <img className="image" src={gamingConsole} alt="gamingConsole" />
+                <input className="button" type="button" value="View Inventory" onClick={this.handleClick} />
+            </div>
+        )
+    }
+}
