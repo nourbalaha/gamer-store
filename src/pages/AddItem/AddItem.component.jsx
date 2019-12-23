49d3f4c8712ea436ee9f2bb4c6021dd48777f9b2
Nour Balaha <nourbalaha0@gmail.com>
2019-12-23 21:03:23 +0900

    implemented add item and connect it to redux

--- src/pages/AddItem/AddItem.component.jsx
+++ src/pages/AddItem/AddItem.component.jsx
@@ -1,89 +1,116 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import './AddItem.style.scss'
+import "./AddItem.style.scss";
 
-export default class AddItem extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            id:"",
-            name:"",
-            platform:"",
-            price: "",
-            image: "",
-            quantity: ""
-        }
-    }
-    
-    handleSubmit=()=>{
+import GameCover from "../../assets/Game Cover Placeholder.jpg"
 
-    }
-    handleChange=(e)=>{
-        this.setState({[e.target.name]:e.target.value})
-    }
-  render () {
+class AddItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: "",
+      name: "",
+      platform: "",
+      price: "",
+      image: "",
+      quantity: ""
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const newItem = this.state;
+    if(!this.state.image) newItem.image=GameCover;
+    this.props.onAddClick(newItem)
+    this.props.history.push("/inventory")
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
     return (
-      <div className='add-item'>
+      <div className="add-item">
         <form className="add-item-form" onSubmit={this.handleSubmit}>
           <span className="title">Add Item</span>
-            <input
+          <input
             className="input"
             name="id"
-            type='text'
+            type="text"
             placeholder="ID"
             value={this.state.id}
             onChange={this.handleChange}
-            />
+            required
+          />
 
-            <input
+          <input
             className="input"
             name="name"
-            type='text'
+            type="text"
             placeholder="Name"
             value={this.state.name}
             onChange={this.handleChange}
-            />
-        
-            <input
+            required
+          />
+
+          <input
             className="input"
             name="platform"
-            type='text'
+            type="text"
             placeholder="Platform"
             value={this.state.platform}
             onChange={this.handleChange}
-            />
-          
-            <input
+            required
+          />
+
+          <input
             className="input"
             name="price"
-            type='text'
+            type="text"
             placeholder="Price"
             value={this.state.price}
             onChange={this.handleChange}
-            />
-          
+            required
+          />
+
             <input
             className="input"
             name="image"
-            type='text'
+            type="text"
             placeholder="Image Url"
             value={this.state.image}
             onChange={this.handleChange}
             />
-          
-            <input
-             className="input"
-             name="quantiny"
-             type='text'
-             placeholder="Quantity"
-            value={this.state.quantiny}
+
+          <input
+            className="input"
+            name="quantity"
+            type="text"
+            placeholder="Quantity"
+            value={this.state.quantity}
             onChange={this.handleChange}
-            />
-          
-          <input className="btn" type='submit' value='Submit' />
+          />
+
+          <input className="btn" type="submit" value="Submit" />
         </form>
       </div>
-    )
+    );
   }
 }
+
+function mapState(state) {
+  return { inventory: state.inventory };
+}
+
+function mapDispatch(dispatch) {
+  return {
+    onAddClick(payload) {
+      dispatch({ type: "ADD_ITEM", payload });
+    }
+  };
+}
+
+export default connect(mapState, mapDispatch)(AddItem);
