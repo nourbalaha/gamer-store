36b16feb1199a9832dacd8907138522ee9f4c7dc
Nour Balaha <nourbalaha0@gmail.com>
2020-01-12 14:08:56 +0900

    wip cart icon

--- src/components/CartIcon/CartIcon.component.jsx
+++ src/components/CartIcon/CartIcon.component.jsx
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
+import { compose } from "redux"
+import { connect } from 'react-redux'
+import { withRouter } from "react-router-dom"
+ 
+import './CartIcon.style.scss'
 
-import "./CartIcon.style.scss"
+import cart from '../../assets/shopping-bag.png'
 
-import cart from "../../assets/shopping-bag.png"
-
-export default class CartIcon extends Component {
-    render() {
-        return (
-            <div className="cart-container">
-                <img className="cart" src={cart} alt="cart" />
-                <span className="cart-counter">5</span>
-            </div>
-        )
-    }
+class CartIcon extends Component {
+  render () {
+    return (
+      <div className='cart-container' onClick={()=>this.props.history.push("/cart")}>
+        <img className='cart' src={cart} alt='cart' />
+        <span className='cart-counter'>{this.props.cart.length<10?this.props.cart.length:"..."}</span>
+      </div>
+    )
+  }
 }
+
+function mapState (state) {
+  return {
+    cart: state.cart.cart
+  }
+}
+
+export default compose(withRouter, connect(mapState, null))(CartIcon)
