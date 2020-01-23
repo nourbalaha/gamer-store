9f7fa5f782c066d0b15ccd494d187700bb871f06
Nour Balaha <nourbalaha0@gmail.com>
2020-01-23 23:41:01 +0900

    added the cart is empty when there is no items in the cart

--- src/pages/Cart/Cart.component.jsx
+++ src/pages/Cart/Cart.component.jsx
@@ -11,12 +11,16 @@ class Cart extends Component {
         return (
             <div className="cart-page">
                 {
+                    keys.length > 0
+                    ?
                     keys.map(item=>{
                         item = this.props.cart[item]
                      return (
                         <CartItem key={item.id} id={item.id} name={item.name} price={item.price} platform={item.platform} quantity={item.quantity} image={item.image} />
                      )   
                     })
+                    :
+                    <h2>The cart is empty</h2>
                 }
             </div>
         )
