9bf5251856a209e23f19113397a76cf52beb7685
Nour Balaha <nourbalaha0@gmail.com>
2020-01-24 23:35:13 +0900

    replaced go to inventory btn with login and register btns

--- src/pages/Home/Home.component.jsx
+++ src/pages/Home/Home.component.jsx
@@ -5,14 +5,20 @@ import "./Home.style.scss"
 import gamingConsole from "../../assets/gaming-console.png"
 
 export default class Home extends Component {
-    handleClick=()=>{
-        this.props.history.push("/inventory")
+    handleLogin=()=>{
+        this.props.history.push("/signin")
+    }
+    handleRegister=()=>{
+        this.props.history.push("/register")
     }
     render() {
         return (
             <div className="home-page">
                 <img className="image" src={gamingConsole} alt="gamingConsole" />
-                <input className="button" type="button" value="View Inventory" onClick={this.handleClick} />
+                <div className="home-btn-container">
+                    <input className="home-btn" type="button" value="LOGIN" onClick={this.handleLogin} />
+                    <input className="home-btn" type="button" value="REGISTER" onClick={this.handleRegister} />
+                </div>
             </div>
         )
     }
