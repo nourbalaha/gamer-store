67c396017953415db701be9376c160867f1f135f
Nour Balaha <nourbalaha0@gmail.com>
2020-02-02 18:11:48 +0900

    added flash msg component

---
+++ src/components/FlashMsg/FlashMsg.component.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { connect } from "react-redux" 
+
+import "./FlashMsg.style.scss"
+
+class FlashMsg extends Component {
+    handleClose=()=>{
+        this.props.deleteMsg({id: this.props.id})
+    }
+    render() {
+        return (
+            <div className={`msg-container ${this.props.type}`}>
+                <span className="msg">{this.props.msg}</span>
+                <span className="msg-close" onClick={this.handleClose}>&times;</span>
+            </div>
+        )
+    }
+}
+
+const mapState = state => {
+    return {
+        flash: state.flash.messages,
+    }
+}
+
+const mapDispatch = dispatch => {
+    return {
+        deleteMsg(payload){
+            dispatch({type: "DELETE_MSG", payload })
+        }
+    }
+}
+
+export default connect(mapState, mapDispatch)(FlashMsg);
+
