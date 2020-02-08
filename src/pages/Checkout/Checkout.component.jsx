095eff949b9895a9d49119a48140c4ca99012313
Nour Balaha <nourbalaha0@gmail.com>
2020-02-08 22:26:13 +0900

    created checkout page

---
+++ src/pages/Checkout/Checkout.component.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import "./Checkout.style.scss"
+
+export default class Checkout extends Component {
+    render() {
+        return (
+            <div className="check-out-container">
+                Checkout
+            </div>
+        )
+    }
+}
