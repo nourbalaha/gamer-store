5cf4ecf9ac248df6e36a3b1bb3a58325779dad6a
Nour Balaha <nourbalaha0@gmail.com>
2020-02-13 00:10:09 +0900

    added stripe payment

--- src/pages/Checkout/Checkout.component.jsx
+++ src/pages/Checkout/Checkout.component.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 
+import StripeButton from "../../components/StripeButton/StripeButton.component"
+
 import "./Checkout.style.scss"
 
 export default class Checkout extends Component {
     render() {
         return (
             <div className="check-out-container">
-                Checkout
+                <div className="test-warning">
+                    *Please use the following test credit card for payment*
+                    <br />
+                    4242 4242 4242 4242 - Exp: 11/23 - CVV: 123
+                </div>
+                <StripeButton price={100} />
             </div>
         )
     }
