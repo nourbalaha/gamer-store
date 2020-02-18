d44dd3196dee54931449197fd108a6e81714f02b
Nour Balaha <nourbalaha0@gmail.com>
2020-02-18 21:29:07 +0900

    refactored navbar component using hooks

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { compose } from "redux";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
@@ -11,57 +11,52 @@ import Dropdown from "../../components/Dropdown/Dropdown.component";
 import CartIcon from "../../components/CartIcon/CartIcon.component";
 import SearchBar from "../../components/SearchBar/SearchBar.component";
 
-class Navbar extends Component {
-  handleSignIn = () => {
-    this.props.history.push("/signin");
-  };
+const Navbar = ({ history, currentUser, admin }) => {
 
-  render() {
-    return (
-      <nav className="navbar">
-        <img
-          className="logo"
-          src={logo}
-          alt="logo"
-          onClick={() => this.props.history.push("/")}
-        />
-        {
-          this.props.currentUser && <SearchBar />
-        }
-        <ul>
-          <li>
-            {
-            this.props.currentUser
-            &&
-            (<span
-              className="nav-item"
-              onClick={() => this.props.history.push("/inventory")}
-            >
-              INVENTORY
-            </span>)
-            }
-          </li>
-          <li>
-            { (this.props.currentUser&&this.props.admin)
-            &&
-            (<span
-              className="nav-item"
-              onClick={() => this.props.history.push("/additem")}
-            >
-              ADD ITEM
-            </span>)
-            }
-          </li>
-          <li>
-            {this.props.currentUser && <Dropdown />}
-          </li>
-          <li>
-            {(this.props.currentUser&&!this.props.admin) && <CartIcon />}
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+  return (
+    <nav className="navbar">
+      <img
+        className="logo"
+        src={logo}
+        alt="logo"
+        onClick={() => history.push("/")}
+      />
+      {
+        currentUser && <SearchBar />
+      }
+      <ul>
+        <li>
+          {
+          currentUser
+          &&
+          (<span
+            className="nav-item"
+            onClick={() => history.push("/inventory")}
+          >
+            INVENTORY
+          </span>)
+          }
+        </li>
+        <li>
+          { (currentUser&&admin)
+          &&
+          (<span
+            className="nav-item"
+            onClick={() => history.push("/additem")}
+          >
+            ADD ITEM
+          </span>)
+          }
+        </li>
+        <li>
+          {currentUser && <Dropdown />}
+        </li>
+        <li>
+          {(currentUser&&!admin) && <CartIcon />}
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 function mapState(state) {
@@ -71,12 +66,4 @@ function mapState(state) {
    };
 }
 
-function mapDispatch(dispatch) {
-  return {
-    onAddUser(payload) {
-      dispatch({ type: "ADD_USER", payload });
-    }
-  };
-}
-
-export default compose(withRouter, connect(mapState, mapDispatch))(Navbar);
+export default compose(withRouter, connect(mapState))(Navbar);
