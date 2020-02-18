6c84f6df1e8f5d6facf82205c0c4340484a28e6b
Nour Balaha <nourbalaha0@gmail.com>
2020-02-18 21:28:47 +0900

    refactored platforms component using hooks

--- src/components/Platforms/Platforms.component.jsx
+++ src/components/Platforms/Platforms.component.jsx
@@ -1,49 +1,48 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 
 import "./Platforms.style.scss"
 
-class Platforms extends Component {
+const Platforms = ({ setPlatform, platform }) => {
 
-    handleClick = platform => {
-        this.props.setPlatform(platform);
+    const handleClick = platform => {
+        setPlatform(platform);
       };
 
-    render() {
-        return (
-            <div className="search-section">
-            <span className="selected-platform">
-              {this.props.platform}
+    return (
+          <div className="search-section">
+          <span className="selected-platform">
+            {platform}
+          </span>
+          <div className="platforms">
+            <span
+              className="platform all"
+              onClick={() => handleClick("All Platforms")}
+            >
+              All Platforms
             </span>
-            <div className="platforms">
-              <span
-                className="platform all"
-                onClick={() => this.handleClick("All Platforms")}
-              >
-                All Platforms
-              </span>
-              <span
-                className="platform"
-                onClick={() => this.handleClick("Playstation 4")}
-              >
-                Playstation 4
-              </span>
-              <span
-                className="platform"
-                onClick={() => this.handleClick("Xbox One")}
-              >
-                Xbox One
-              </span>
-              <span
-                className="platform"
-                onClick={() => this.handleClick("Nintendo Switch")}
-              >
-                Nintendo Switch
-              </span>
-            </div>
+            <span
+              className="platform"
+              onClick={() => handleClick("Playstation 4")}
+            >
+              Playstation 4
+            </span>
+            <span
+              className="platform"
+              onClick={() => handleClick("Xbox One")}
+            >
+              Xbox One
+            </span>
+            <span
+              className="platform"
+              onClick={() => handleClick("Nintendo Switch")}
+            >
+              Nintendo Switch
+            </span>
           </div>
-        )
-    }
+        </div>
+      )
+    
 }
 
 
