33b425196dd0d95cd507dae37690cdc26e52ca56
Nour Balaha <nourbalaha0@gmail.com>
2020-02-18 21:29:30 +0900

    refactored item component using hooks

--- src/components/Item/Item.component.jsx
+++ src/components/Item/Item.component.jsx
@@ -1,20 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import { withRouter } from "react-router-dom";
 
 import "./Item.style.scss";
 
-class Item extends Component {
-  render() {
-    const { id, name, price, image, platform } = this.props;
-    return (
-      <div onClick={()=>this.props.history.push(`/inventory/${id}`)} className="item">
-        <img className="item-image" src={image} alt={name} />
-        <span className="item-name">{name}</span>
-        <span className="item-platform">{platform}</span>
-        <span className="item-price">{price}$</span>
-      </div>
-    );
-  }
+const Item = ({ id, name, price, image, platform, history }) => {
+  return (
+    <div onClick={()=>history.push(`/inventory/${id}`)} className="item">
+      <img className="item-image" src={image} alt={name} />
+      <span className="item-name">{name}</span>
+      <span className="item-platform">{platform}</span>
+      <span className="item-price">{price}$</span>
+    </div>
+  );
 }
 
 export default withRouter(Item);
