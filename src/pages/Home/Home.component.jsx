76308179f809908a44c62f9345a8bcabdc6dcec8
Nour Balaha <nourbalaha0@gmail.com>
2020-02-18 21:25:51 +0900

    refactored home component using hooks

--- src/pages/Home/Home.component.jsx
+++ src/pages/Home/Home.component.jsx
@@ -1,22 +1,31 @@
-import React, { Component } from 'react'
- 
-import "./Home.style.scss"
+import React from 'react'
+import PropTypes from "prop-types"
 
-export default class Home extends Component {
-    handleLogin=()=>{
-        this.props.history.push("/signin")
-    }
-    handleRegister=()=>{
-        this.props.history.push("/register")
-    }
-    render() {
-        return (
-            <div className="home-page">
-                <div className="home-btn-container">
-                    <button className="home-btn" onClick={this.handleLogin}>LOGIN</button>
-                    <button className="home-btn" onClick={this.handleRegister} >REGISTER</button>
-                </div>
-            </div>
-        )
-    }
+import './Home.style.scss'
+
+const Home = props => {
+  const handleLogin = () => {
+    props.history.push('/signin')
+  }
+  const handleRegister = () => {
+    props.history.push('/register')
+  }
+  return (
+    <div className='home-page'>
+      <div className='home-btn-container'>
+        <button className='home-btn' onClick={handleLogin}>
+          LOGIN
+        </button>
+        <button className='home-btn' onClick={handleRegister}>
+          REGISTER
+        </button>
+      </div>
+    </div>
+  )
 }
+
+Home.propTypes = {
+    history: PropTypes.object,
+}
+
+export default Home
