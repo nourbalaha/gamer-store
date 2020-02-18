206a7ef8a9b9e03b1d820625421a1a607c2cb119
Nour Balaha <nourbalaha0@gmail.com>
2020-02-18 21:31:53 +0900

    refactored cart icon component using hooks

--- src/components/CartIcon/CartIcon.component.jsx
+++ src/components/CartIcon/CartIcon.component.jsx
@@ -1,26 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { compose } from "redux"
 import { connect } from 'react-redux'
 import { withRouter } from "react-router-dom"
  
 import './CartIcon.style.scss'
 
-import cart from '../../assets/shopping-bag.png'
+import shoppingBag from '../../assets/shopping-bag.png'
 
-class CartIcon extends Component {
-  render () {
-    let keys = Object.keys(this.props.cart);
-    let numberOfItems = 0;
-    if(keys.length>0){
-      numberOfItems = keys.map(key=>this.props.cart[key].quantity).reduce((a,b)=>a+b);
-    }
-    return (
-      <div className='cart-container' onClick={()=>this.props.history.push("/cart")}>
-        <img className='cart' src={cart} alt='cart' />
-        <span className='cart-counter'>{numberOfItems<10?numberOfItems:"..."}</span>
-      </div>
-    )
+const CartIcon = ({ cart, history }) => {
+  const keys = Object.keys(cart);
+  let numberOfItems = 0;
+  if(keys.length > 0){
+    numberOfItems = keys.map(key => cart[key].quantity).reduce((a, b) => a + b);
   }
+  return (
+    <div className='cart-container' onClick={() => history.push("/cart")}>
+      <img className='cart' src={shoppingBag} alt='cart-icon' />
+      <span className='cart-counter'>{numberOfItems < 10 ? numberOfItems : "..."}</span>
+    </div>
+  )
 }
 
 function mapState (state) {
@@ -29,4 +27,4 @@ function mapState (state) {
   }
 }
 
-export default compose(withRouter, connect(mapState, null))(CartIcon)
+export default compose(withRouter, connect(mapState))(CartIcon)
